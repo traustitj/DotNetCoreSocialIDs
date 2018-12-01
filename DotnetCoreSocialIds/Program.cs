84037bf9c1033a113f0bcc02cd6e93934979cb53
Trausti Thor Johannsson <traustitj@mac.com>
2018-12-01 19:35:11 +0100

    adding a parallel main async

--- DotnetCoreSocialIds/Program.cs
+++ DotnetCoreSocialIds/Program.cs
@@ -6,31 +6,54 @@ namespace DotnetCoreSocialIds
 {
     class Program
     {
+        public static async Task MainAsyncParallel(string[] args)
+        {
+            var s = new SocialIDS();
+            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+            List<Task<List<string>>> tasks = new List<Task<List<String>>>();
+
+            for (int i = 1900; i <= 2000; i++)
+            {
+                tasks.Add(Task.Run(() => s.GenerateAllWithChecksum(i)));
+                
+            }
+
+
+            var results = await Task.WhenAll(tasks);
+            foreach (var item in results)
+            {
+                Console.WriteLine($"I generated {item.Count}");
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"This took {stopWatch.ElapsedMilliseconds} ms");
+            Console.ReadKey();
+        }
+
         public static async Task MainAsync(string[] args)
         {
             var s = new SocialIDS();
-
+            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
             List<List<string>> tasks = new List<List<String>>();
 
             for (int i = 1900; i <= 2000; i++)
             {
-                var longRun = await Task.Run(() => s.GenerateAllWithChecksum(i));
-                tasks.Add(longRun);
+                tasks.Add(await Task.Run(() => s.GenerateAllWithChecksum(i)));
             }
 
-
-            var year = 1900;
             foreach (var item in tasks)
             {
-                Console.WriteLine($"I generated {item.Count} for year {year}");
-                    year++;
+                Console.WriteLine($"I generated {item.Count}");
             }
 
+            stopWatch.Stop();
+            Console.WriteLine($"This took {stopWatch.ElapsedMilliseconds} ms");
+            Console.ReadKey();
         }
 
         public static void Main(string[] args)
         {
-            MainAsync(args).GetAwaiter().GetResult();
+            MainAsyncParallel(args).GetAwaiter().GetResult();
 
         }
     }
