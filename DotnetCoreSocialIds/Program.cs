41e1e3caa66338b454ca7c4e48dbb3952ce44aca
Trausti Thor Johannsson <traustitj@mac.com>
2024-10-11 00:31:37 +0000

    Added new ways to run the app. Changed year from 2000 to 2024

--- DotnetCoreSocialIds/Program.cs
+++ DotnetCoreSocialIds/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
+using System.Linq;
 namespace DotnetCoreSocialIds
 {
     class Program
@@ -17,17 +18,39 @@ namespace DotnetCoreSocialIds
 
             await Task.Run(() =>
             {
-                Parallel.For(1900, 2000, i =>
+                Parallel.For(1900, 2024, i =>
                 {
                     var x = s.GenerateAllWithChecksum(i);
                     results.Add(x);
-                    Console.WriteLine($"Generated for year {i} total count {x.Count}");
+                    //Console.WriteLine($"Generated for year {i} total count {x.Count}");
                 });
             });
 
             stopWatch.Stop();
-            Console.WriteLine($"This took {stopWatch.ElapsedMilliseconds} ms");
-            Console.ReadKey();
+            var total = results.SelectMany(x => x).Count();
+            Console.WriteLine($"This took {stopWatch.ElapsedMilliseconds} ms and created {total} social ids");
+        }
+
+        public static async Task NewMainSyncParallel(string[] args)
+        {
+            var s = new NewSocialIds();
+            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+            
+            var results = new List<List<List<int>>>();
+
+            await Task.Run(() =>
+            {
+                Parallel.For(1900, 2024, i =>
+                {
+                    var x = s.GenerateAllPossibleIds(i);
+                    results.Add(x);
+                    //Console.WriteLine($"Generated for year {i} total count {x.Count}");
+                });
+            });
+
+            stopWatch.Stop();
+            var total = results.SelectMany(x => x).Count();
+            Console.WriteLine($"New way took {stopWatch.ElapsedMilliseconds} ms and created {total} social ids");
         }
 
 
@@ -37,7 +60,7 @@ namespace DotnetCoreSocialIds
             var stopWatch = System.Diagnostics.Stopwatch.StartNew();
             List<Task<List<string>>> tasks = new List<Task<List<String>>>();
 
-            for (int i = 1900; i <= 2000; i++)
+            for (int i = 1900; i <= 2024; i++)
             {
                 tasks.Add(Task.Run(() => s.GenerateAllWithChecksum(i)));
                 
@@ -78,8 +101,20 @@ namespace DotnetCoreSocialIds
 
         public static void Main(string[] args)
         {
-            MainSyncParallel(args).GetAwaiter().GetResult();
-
+            //MainSyncParallel(args).GetAwaiter().GetResult();
+            //NewMainSyncParallel(args).GetAwaiter().GetResult();
+            //var s = new SocialIDS();
+            //var ns = new NewSocialIds();
+            //var stopw1 = System.Diagnostics.Stopwatch.StartNew();
+            //var x = s.GenerateAllWithChecksum(2024);
+            //stopw1.Stop();
+            //Console.WriteLine($"Old way took {stopw1.ElapsedMilliseconds} ms, creating a total of {x.Count} days");
+            //var stopw2 = System.Diagnostics.Stopwatch.StartNew();
+            //var y = ns.GenerateAllPossibleIds(2024);
+            //stopw2.Stop();
+            
+            //Console.WriteLine($"New way took {stopw2.ElapsedMilliseconds} ms, creating a total of {y.Count} days");
+            BenchmarkRunner.Run<Benchmarks>();
         }
     }
 }
