660e1f54bf509c516eb83bb3a0e2ad93e663138a
Trausti Thor Johannsson <traustitj@mac.com>
2024-10-11 00:29:46 +0000

    Adding new benchmarks, because why not

---
+++ DotnetCoreSocialIds/Benchmarks.cs
@@ -0,0 +1,29 @@
+﻿using DotnetCoreSocialIds;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+namespace DotnetCoreSocialIds
+{
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void OldGenerateDaysForYear()
+        {
+            var s = new SocialIDS();
+            s.GenerateAllWithChecksum(2024);
+        }
+
+        [Benchmark]
+        public void NewGenerateDaysOfYear()
+        {
+            var ns = new NewSocialIds();
+            ns.GenerateAllPossibleIds(2024);
+        }
+
+        
+
+    }
+}
